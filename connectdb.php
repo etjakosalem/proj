f5e790b96dc5bcd5ec7a6f2a23e57b3306ca2a25
Jakosalem <etjakosalem.student@asiancollege.edu.ph>
2026-03-03 21:01:46 +0800

    Update connectdb.php

--- connectdb.php
+++ connectdb.php
@@ -1,7 +1,8 @@
 <?php
-$connection = mysqli_connect("localhost", "root", "", "todo_db");
+$connection = mysqli_connect("localhost", "root", "", "floral-math-22355198");
 
 if (!$connection) {
     die("Connection failed: " . mysqli_connect_error());
 }
+
 ?>
